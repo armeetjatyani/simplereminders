322c97871b9ac2cc3f7a003bcb6460b9908334a7
Armeet Singh Jatyani <armeetjatyani@gmail.com>
2022-01-30 17:16:11 -0800

    responsive

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ export default function App() {
 	return (
 		<div className="App">
 			<div className="bg-slate-200 w-screen h-screen flex justify-center items-center">
-				<div className="bg-white shadow-xl w-[90%] sm:w-[500px] p-8 rounded-xl">
+				<div className="bg-white shadow-xl w-[90%] md:w-[500px] p-8 rounded-xl">
+				{/* <div className="bg-white shadow-xl w-[500px] min-w-0 p-8 rounded-xl"> */}
 					<Reminders />
 				</div>
 			</div>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./index.css";
 
 ReactDOM.render(
 	<React.StrictMode>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 		<div className="antialiased">
 			<App />
 		</div>
