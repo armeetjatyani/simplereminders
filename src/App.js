25dbec241c5f7d32dfc70cd6da0bfd90f1cf6f41
Armeet Singh Jatyani <armeetjatyani@gmail.com>
2022-01-30 16:59:19 -0800

    responsive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Reminders from "./components/Reminders"
+import Reminders from "./components/Reminders";
 
 export default function App() {
 	return (
 		<div className="App">
-			<div className="bg-slate-200 w-full h-full flex justify-center items-center">
-        <div className="bg-white shadow-md w-[500px] p-8">
-          <Reminders />
+			<div className="bg-slate-200 w-screen h-screen flex justify-center items-center">
+				<div className="bg-white shadow-xl w-[90%] sm:w-[500px] p-8 rounded-xl">
+					<Reminders />
 				</div>
 			</div>
 		</div>
-	)
+	);
 }
