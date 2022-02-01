7a885a28a716912dee577983ecdcce4aabac57d5
Armeet Singh Jatyani <armeetjatyani@gmail.com>
2022-01-31 17:32:53 -0800

    added max height (scroll if there are too many tasks)

--- src/components/Reminders.js
+++ src/components/Reminders.js
@@ -100,22 +100,24 @@ export default function Reminders() {
 			{/* List */}
 			<ul className="divide-gray-200">
 				{reminders.length === 0 && (
-					<p className="italic text-gray-500 select-none">
+					<p className="italic text-center text-gray-500 select-none">
 						No reminders! Add your own or{" "}
-						<a className="font-semibold cursor-pointer text-violet-600" onClick={() => setReminders(defaultData.reminders)}>
+						<a className="font-semibold cursor-pointer text-violet-600 hover:underline" onClick={() => setReminders(defaultData.reminders)}>
 							import defaults
 						</a>
 					</p>
 				)}
-				<Reorder.Group axis="y" onReorder={setReminders} values={reminders}>
-					{reminders.map((element) => {
-						return (
-							<Reorder.Item key={element.id} value={element}>
-								<Task element={element} deleteReminder={deleteReminder} />
-							</Reorder.Item>
-						);
-					})}
-				</Reorder.Group>
+				<div className="max-h-[60%] overflow-y-auto overflow-x-clip">
+					<Reorder.Group axis="y" onReorder={setReminders} values={reminders}>
+						{reminders.map((element) => {
+							return (
+								<Reorder.Item key={element.id} value={element}>
+									<Task element={element} deleteReminder={deleteReminder} />
+								</Reorder.Item>
+							);
+						})}
+					</Reorder.Group>
+				</div>
 				<li className="flex items-center justify-between py-2">
 					<form className={`${showForm ? "block" : "hidden"} flex justify-between items-center w-full relative`} onSubmit={addReminder}>
 						<input autoComplete="false" autoFocus={true} id="input" className="relative block w-full px-2 py-1 outline-violet-600/60" type="text" placeholder="New reminder..."></input>
@@ -141,19 +143,19 @@ export default function Reminders() {
 			<div className="flex flex-col items-center justify-center mt-10">
 				<p>
 					Made by{" "}
-					<a className="font-semibold text-violet-600" href="https://armeet.com" target="_blank" rel="noreferrer">
+					<a className="font-semibold text-violet-600 hover:underline" href="https://armeet.com" target="_blank" rel="noreferrer">
 						@armeetjatyani
 					</a>
 				</p>
 				<p className="flex flex-col items-center justify-center mt-4 text-gray-600">
 					<div>
-						<kbd className="px-2 font-mono font-bold bg-gray-300 rounded-md">Tab</kbd> - New Reminder<br></br>
+						<kbd className="px-2 font-mono font-bold bg-gray-300 rounded-md hover:underline hover:cursor-pointer">Tab</kbd> - New Reminder<br></br>
 					</div>
 					<div>
-						<kbd className="px-2 font-mono font-bold bg-gray-300 rounded-md">Backspace</kbd> - Delete Last Reminder<br></br>
+						<kbd className="px-2 font-mono font-bold bg-gray-300 rounded-md hover:underline hover:cursor-pointer">Backspace</kbd> - Delete Last Reminder<br></br>
 					</div>
 					<div>
-						<kbd className="px-2 font-mono font-bold bg-gray-300 rounded-md">Esc</kbd> - Cancel New Reminder
+						<kbd className="px-2 font-mono font-bold bg-gray-300 rounded-md hover:underline hover:cursor-pointer">Esc</kbd> - Cancel New Reminder
 					</div>
 				</p>
 			</div>
